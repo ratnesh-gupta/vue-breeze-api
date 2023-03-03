aa221e4c4911726c0650ec27200c053e232df500
Ratnesh Gupta <ratnesh@webinarninja.com>
2023-03-03 13:57:47 +0530

    Router updated with main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createPinia} from "pinia";
+import router from './router';
+
+import './axios'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia)
+    .use(router)
+    .mount('#app')
